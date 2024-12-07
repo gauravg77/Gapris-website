65aae5ec660d66dda8234e0ab468e8d51ce3c1f8
gauravg77 <114395211+gauravg77@users.noreply.github.com>
2024-12-07 21:56:04 +0545

    changes in validation code ( still issues with validation)

--- WEB/dbconnect.php
+++ WEB/dbconnect.php
@@ -1,14 +1,7 @@
-<?php
-$host = "localhost";
-$dbname = "authorization";
-$username = "root";
-$password = "";
+<?php 
+$host="localhost";
+$dbname='authorization';
+$username='root';
+$password='';
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to database successfully!";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
+$pdo=new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
